281c7ded57854a834a4375474d5b7c521f1bbb5c
seb <sebastienlorandel@gmail.com>
2018-12-09 10:41:46 -0500

    initial commit

---
+++ AngularJsAspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJsAspNet.Startup))]
+namespace AngularJsAspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
